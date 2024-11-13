148f89701ea94ddefe2100d84601c44b1a62d104
ZamTheCoder <rudhav935@gmail.com>
2024-11-13 20:02:21 +0530

    first commit

---
+++ Bday.py
@@ -0,0 +1,12 @@
+Raj = "October 5th"
+
+Rudhav = "November 22nd"
+
+Ramesh = "July 4th"
+
+Bharata = "15th August"
+
+print("Raj's Birthday is on", Raj)
+print("Rudhavs's Birthday is on", Rudhav)
+print("Ramesh's Birthday is on", Ramesh)
+print("Bhararta's Birthday is on", Bharata)
